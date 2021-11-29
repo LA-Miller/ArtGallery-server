80e7d2abf608a47b355ce409adf8eb1fee03f120
Marcus Schnake <marcus.schnake@gmail.com>
2021-11-29 00:56:39 -0500

    created artGallery endpoint

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const Express = require("express");
 const app = Express();
 
+const controllers = require("./controllers");
+
+app.use("/artGallery", controllers.artGalleryController);
+
 app.listen(3000, () => {
     console.log(`[Server]: App is listening on 3000.`);
-})
+});
+
