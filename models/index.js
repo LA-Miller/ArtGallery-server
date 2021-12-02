1e130c0ca01f3f9f8e406dfb83d1fb935a6fc112
LA-Miller <lamiller516@gmail.com>
2021-12-01 21:53:36 -0500

    Fixed some issues after pulling

--- models/index.js
+++ models/index.js
@@ -1,10 +1,6 @@
 const UserModel = require('./user');
 const PostModel = require('./art');
 
-
-module.exports = {
-const PostModel = require("./art");
-
 module.exports = { 
     UserModel,
     PostModel
