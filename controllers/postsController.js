e9e75c3251890ea66fe1365393ca96b68a50d670
Luke Miller <lamiller516@gmail.com>
2021-12-13 23:02:00 -0500

    Added functionality to get all posts of a single user

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -10,32 +10,33 @@ const { PostModel } = require("../models");
 http://localhost:3000/art/create
 */
 router.post("/create", validateJWT, async (req, res) => {
-    const { artist_name, url, description, style, era, for_sale, price } = req.body.post;
-    const { id } = req.user;
-    const email = req.email;
+  const { artist_name, url, description, style, era, for_sale, price } =
+    req.body.post;
+  const { id } = req.user;
+  const email = req.email;
 
-    const forSale = for_sale === "on" ? true : false;
-    const priceInt = parseInt(price);
-    console.log(priceInt);
-    try {
-        const newPost = await PostModel.create({
-            artist_name,
-            url,
-            description,
-            style,
-            era,
-            for_sale: forSale,
-            price: priceInt,
-            owner_id: id,
-            email: email,
-        });
-        res.status(201).json({
-            message: "Post successful",
-        })
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({ error: err });
-    }
+  const forSale = for_sale === "on" ? true : false;
+  const priceInt = parseInt(price);
+  console.log(priceInt);
+  try {
+    const newPost = await PostModel.create({
+      artist_name,
+      url,
+      description,
+      style,
+      era,
+      for_sale: forSale,
+      price: priceInt,
+      owner_id: id,
+      email: email,
+    });
+    res.status(201).json({
+      message: "Post successful",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err });
+  }
 });
 
 /*
@@ -46,12 +47,28 @@ http://localhost:3000/art/
 */
 
 router.get("/", validateJWT, async (req, res) => {
-    try {
-        const userPosts = await PostModel.findAll();
-        res.status(200).json(userPosts);
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
+  try {
+    const userPosts = await PostModel.findAll();
+    res.status(200).json(userPosts);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+// GET ALL POSTS OF AN INDIVIDUAL USER
+router.get("/user", validateJWT, async (req, res) => {
+  const { id } = req.user;
+
+  try {
+    const userPosts = await PostModel.findAll({
+      where: {
+        owner_id: id,
+      },
+    });
+    res.status(200).json(userPosts);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 /*
@@ -61,32 +78,33 @@ router.get("/", validateJWT, async (req, res) => {
 http://localhost:3000/art/update/:entryid
 */
 router.put("/update/:entryId", validateJWT, async (req, res) => {
-    const { artist_name, url, description, style, era, for_sale, price } = req.body.art;
-    const postId = req.params.entryId;
-    const userId = req.user.id;
+  const { artist_name, url, description, style, era, for_sale, price } =
+    req.body.art;
+  const postId = req.params.entryId;
+  const userId = req.user.id;
 
-    const query = {
-        where: {
-            id: postId,
-            owner_id: userId
-        }
-    };
-    const updatePost = {
-        artist_name: artist_name,
-        url: url,
-        description: description,
-        style: style,
-        era: era,
-        for_sale: for_sale,
-        price: price
-    };
-    try {
-        const update = await PostModel.update(updatePost, query);
-        res.status(200).json(update);
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
-})
+  const query = {
+    where: {
+      id: postId,
+      owner_id: userId,
+    },
+  };
+  const updatePost = {
+    artist_name: artist_name,
+    url: url,
+    description: description,
+    style: style,
+    era: era,
+    for_sale: for_sale,
+    price: price,
+  };
+  try {
+    const update = await PostModel.update(updatePost, query);
+    res.status(200).json(update);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
 
 /*
 ========================================
@@ -95,22 +113,22 @@ router.put("/update/:entryId", validateJWT, async (req, res) => {
 http://localhost:3000/art/:id
 */
 router.delete("/:id", validateJWT, async (req, res) => {
-    const ownerId = req.user.id;
-    const postId = req.params.id;
+  const ownerId = req.user.id;
+  const postId = req.params.id;
 
-    try {
-        const query = {
-            where: {
-                id: postId,
-                owner_id: ownerId,
-            },
-        };
+  try {
+    const query = {
+      where: {
+        id: postId,
+        owner_id: ownerId,
+      },
+    };
 
-        await PostModel.destroy(query);
-        res.status(200).json({ message: "Art Entry Removed" });
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
+    await PostModel.destroy(query);
+    res.status(200).json({ message: "Art Entry Removed" });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 module.exports = router;
